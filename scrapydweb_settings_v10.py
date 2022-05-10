592cd147f22d336188111e9d12dd7ef4e3ab0cb7
Merci Jacob <mercihabam@gmail.com>
2022-05-10 07:08:12 +0200

    update scrapyd config file

--- scrapydweb_settings_v10.py
+++ scrapydweb_settings_v10.py
@@ -51,7 +51,7 @@ SCRAPYD_SERVERS = [
     # 'username:password@localhost:6801#group',
     '0.0.0.0:6800',
     'host.docker.internal:6800',
-    ('username', 'password', 'localhost', '6801', 'group'),
+    ('root', 'bVP4H05!o1', '89.40.12.167', '6800', 'group'),
 ]
 
 
