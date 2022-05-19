d49d86ebc542faff43db43e66344f5261436396c
Merci Jacob <mercihabam@gmail.com>
2022-05-19 19:53:18 +0200

    add scooprdc spider

---
+++ src/scrapers/spiders/scooprdc.py
@@ -0,0 +1,27 @@
+from scrapy.spiders import Rule
+from src.scrapers.spiders.base import BaseSpider
+
+
+class ScooprdcSpider(BaseSpider):
+    name = 'scooprdc'
+    website_origin = 'https://scooprdc.net'
+    start_urls = ['https://scooprdc.net/']
+    allowed_domains = ['scooprdc.net']
+
+    rules = (
+        Rule(callback='callback', follow=True),
+    )
+
+    def callback(self, response):
+        title_path = '.tdb-title-text::text'
+        content_path = '.td-post-content * p::text'
+        author_path = '.tdb-author-name::text'
+        posted_at_path = '.td-module-date::attr(datetime)'
+        sumary_path = '.sumary'
+        return self.default_parser(response, css_paths={
+            'title_path': title_path,
+            'content_path': content_path,
+            'sumary_path': sumary_path,
+            'posted_at_path': posted_at_path,
+            'author_path': author_path
+        })
